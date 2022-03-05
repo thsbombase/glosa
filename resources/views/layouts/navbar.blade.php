13f25af4f9d1af043e1ab8036dfec4f7920a9a82
Tristan <tristan.bombase13@gmail.com>
2022-03-05 13:33:07 +0800

    navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class=" border-gray-200 px-2 sm:px-4 py-2.5 bg-gray-800">
+    <div class="container flex flex-wrap items-center justify-between mx-auto">
+        <a href="#" class="flex items-center">
+            <span class="self-center text-xl font-semibold text-white whitespace-nowrap">Glósa</span>
+        </a>
+        <div class="flex md:order-2">
+            <button type="button"
+                class="px-3 py-2 mr-2 text-xs font-medium text-center text-white rounded-lg dark:bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Login</button>
+            <button type="button"
+                class="px-3 py-2 mr-3 text-xs font-medium text-center text-white rounded-lg md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-blue-800">Signup</button>
+        </div>
+    </div>
+</nav>
